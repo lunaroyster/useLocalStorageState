a750ad0a9be88434ed02d20fedcd3c5ba7d215db
Arnav Bansal <lunaroyster@users.noreply.github.com>
2021-10-31 11:55:11 -0400

    better typing

--- index.tsx
+++ index.tsx
@@ -1,23 +1,52 @@
-import {createContext, useContext, useReducer, useCallback, useEffect} from 'react';
+import {
+  createContext,
+  useContext,
+  useReducer,
+  useCallback,
+  useEffect,
+} from "react";
 
-const LocalStorageContext = createContext({ setItem: () => { }, getItem: () => { } });
+export type StateType = Record<string, any>;
+
+export type ReducerActionType =
+  | { type: "set"; key: string; value: any }
+  | { type: "init"; key: string; value: any }
+  | { type: "clear"; key: string; value: any };
+export interface LocalStorageContextState {
+  state: StateType;
+  getItem: <T>(key: string) => T;
+  setItem: <T>(key: string, initialValue: T) => void;
+  initItem: <T>(key: string, initialValue: T) => void;
+}
+
+const LocalStorageContext = createContext<LocalStorageContextState>({
+  state: {},
+  setItem: () => {},
+  getItem: () => null,
+  initItem: () => {},
+});
 
 export function useLocalStorageState<T>(name: string, initialValue: T) {
   const { state, setItem, initItem } = useContext(LocalStorageContext);
 
   useEffect(() => {
-    initItem(name, initialValue)
-  }, [])
+    initItem(name, initialValue);
+  }, []);
 
-  const setValue = useCallback((value) => {
-    setItem(name, value);
-  }, [setItem])
+  const setValue = useCallback(
+    (value) => {
+      setItem(name, value);
+    },
+    [setItem]
+  );
 
   return [state[name], setValue];
 }
 
 export function LocalStorageContextProvider({ children }) {
-  const [state, dispatch] = useReducer((state, action) => {
+  const [state, dispatch] = useReducer<
+    (s: StateType, action: ReducerActionType) => StateType
+  >((state, action) => {
     if (action.type === "set") {
       return { ...state, [action.key]: action.value };
     } else if (action.type === "init") {
@@ -34,27 +63,34 @@ export function LocalStorageContextProvider({ children }) {
     }
   }, {});
 
-  const setItem = useCallback((key: string, value) => {
+  const setItem = useCallback((key: string, value: any) => {
     window.localStorage.setItem(key, JSON.stringify(value));
     dispatch({ type: "set", key, value });
   }, []);
 
-  const getItem = useCallback(
+  const getItem: <T>(key: string) => T = useCallback(
     (key: string) => {
       return state[key];
     },
     [state]
   );
 
-  const initItem = useCallback((key: string, initialValue) => {
-    if (state[key]) {
-      return;
-    }
+  const initItem = useCallback(
+    function <T>(key: string, initialValue: T) {
+      if (state[key]) {
+        return;
+      }
 
-    const existingValue = window.localStorage.getItem(key);
+      const existingValue = window.localStorage.getItem(key);
 
-    dispatch({ type: "init", key, value: JSON.parse(existingValue) ?? initialValue});
-  }, [state])
+      dispatch({
+        type: "init",
+        key,
+        value: JSON.parse(existingValue) ?? initialValue,
+      });
+    },
+    [state]
+  );
 
   return (
     <LocalStorageContext.Provider value={{ state, setItem, getItem, initItem }}>
